f6c5476d7dacdf7d5c66dbaa4446109ed4e34440
joker <569957919@qq.com>
2019-08-31 19:13:25 +0800

    commit

---
+++ itoken-config/src/main/java/com/zmt/ItokenConfigApplication.java
@@ -0,0 +1,15 @@
+package com.zmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ItokenConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenConfigApplication.class, args);
+    }
+}
